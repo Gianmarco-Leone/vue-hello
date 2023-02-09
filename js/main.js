ad720766eb7066158de5ab7ff76ff9b1a3479fad
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-02-09 15:01:19 +0100

    add bonus: add image with Vue

--- js/main.js
+++ js/main.js
@@ -14,7 +14,10 @@ const app = Vue.createApp({
     data() {
         return {
             // Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
-            messageTitle: "I primi passi con Vue.js"
+            messageTitle: "I primi passi con Vue.js",
+
+            // BONUS - Aggiungere alla pagina un'immagine, presa anch'essa da un data.
+            image: "./img/01.webp"
         }
     }
 });
