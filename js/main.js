83063ab6bd074b9f9ed1f0da4eb60a693b7e467b
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-02-09 14:41:08 +0100

    initialise Vue

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@
 // ? Bonus:
 // Aggiungere alla pagina un'immagine, presa anch'essa da un data.
 
+// *SVOLGIMENTO:
+
+// Controllo se Vue è collegato correttamente
+console.log(Vue);
+
+// Faccio funzionare Vue dichiarando una variabile locale collegata alla variabile globale Vue
+const app = Vue.createApp({});
+
+// Monto Vue dentro il div contenitore dell'html
+app.mount("#root");
